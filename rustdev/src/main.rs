b973239dee2549fa67a440e391e9b2fe89ad8de0
unknown <nomispus@icloud.com>
2025-05-12 22:27:03 +0200

    rust_and_ada_dev_start

---
+++ rustdev/src/main.rs
@@ -0,0 +1,8 @@
+
+fn main() {
+    let mut counter: i32 = 1;
+    while counter <= 100000 {
+        println!("Hello, world! {}", counter);
+        counter += 1;
+    }
+}
---
+++ rustdev/target/debug/rusttest.d
@@ -0,0 +1,1 @@
+C:\Users\MAU\.vscode\amundworks\rustdev\target\debug\rusttest.exe: C:\Users\MAU\.vscode\amundworks\rustdev\src\main.rs
