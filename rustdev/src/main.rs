a71a2404c38cf976c1a6545be6fc160e904ea9ec
unknown <nomispus@icloud.com>
2025-05-13 09:57:56 +0200

    rustdev

--- rustdev/src/main.rs
+++ rustdev/src/main.rs
@@ -1,8 +1,20 @@
 
+
+fn has3asfactor(num: i32) -> bool {
+    num % 3 == 0
+}
 fn main() {
     let mut counter: i32 = 1;
     while counter <= 100000 {
-        println!("Hello, world! {}", counter);
+        if counter % 2 == 0 {
+            println!("{} is even", counter);
+        } else {
+            println!("{} is odd", counter);
+        }
+        if has3asfactor(counter) {
+                println!("Number is divisible by 3");
+            }
         counter += 1;
     }
+    println!("Ferdig"); 
 }
