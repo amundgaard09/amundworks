0c24676a51ee5913aa296902997320a75fa65ada
amundgaard09 <nomispus@icloud.com>
2026-03-16 09:21:45 +0100

    UniTrig Update - 16.03.2026

--- Python/Utilities/UniTrig.py
+++ Python/Utilities/UniTrig.py
@@ -1,13 +1,35 @@
-### First Iteration - Amundworks Unified Trigonometry Calculation Platform
+### UniTrig x1 - AmundWorks Unified Trigonometry Calculation Platform
 
 import math
 
 def CosineRule(LengthA: float, LengthB: float, AngleA: float) -> float:
     return math.sqrt(LengthA ** 2 + LengthB ** 2 - ((2 * LengthA * LengthB) * math.cos(math.radians(AngleA))))
-
 def ReverseCosineRule(LengthA: float, LengthB: float, LengthC: float) -> float:
-    return math.degrees(math.acos((LengthA ** 2 + LengthB ** 2 - LengthC ** 2) / (2 * LengthA * LengthB)))
+    """ 
+    Returns a tuple of the three angles in degrees, in the order of AngleA, AngleB, AngleC 
+    
+    Formula: AngleA = arccos((B^2 + C^2 - A^2) / (2BC))
+    """
 
-def TriangleArea(LengthA: float, LengthB: float, AngleC: float) -> float:
+    return (
+        math.degrees(math.acos((LengthB ** 2 + LengthC ** 2 - LengthA ** 2) / (2 * LengthB * LengthC))),  # AngleA
+        math.degrees(math.acos((LengthC ** 2 + LengthA ** 2 - LengthB ** 2) / (2 * LengthC * LengthA))),  # AngleB
+        math.degrees(math.acos((LengthA ** 2 + LengthB ** 2 - LengthC ** 2) / (2 * LengthA * LengthB)))   # AngleC
+    )
+
+def ConventionalTriangleArea(LengthA: float, LengthB: float, AngleC: float) -> float:
     return (0.5 * LengthA * LengthB * math.sin(math.radians(AngleC)))
+def HeronsFormula(LengthA: float, LengthB: float, LengthC: float) -> float:
+    """
+    Returns the area of a triangle from the side lengths.
 
+    Args:
+        LenghtA (float):
+        LenghtB (float):
+        LenghtC (float):
+
+    Returns:
+        Area (float):
+    """
+    SemiPerimiter = (LengthA + LengthB + LengthC) / 2
+    return math.sqrt(SemiPerimiter * (SemiPerimiter - LengthA) * (SemiPerimiter - LengthB) * (SemiPerimiter - LengthC))
