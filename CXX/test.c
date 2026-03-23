f94b7a9b09598726b60d0aed6293e8d5ca2c0ea8
amundgaard09 <nomispus@icloud.com>
2026-03-23 09:35:05 +0100

    C - Startup

---
+++ CXX/test.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+// recursion function tests
+
+uint64_t factorial(int i) {
+    if (i == 0 || i == 1) {
+        return 1;
+    } else {
+        return (i * factorial(i - 1));
+    }
+}
+
+uint64_t fibonacci(int i) {
+    if (i==0) {
+        return 0;
+    } else if (i==1) {
+        return 1;
+    } else {
+    return fibonacci(i-1) + fibonacci(i-2);
+    }
+}
+
+int main() {
+    int f1, f2;
+
+    printf("Enter factorial: ");
+    scanf("%d", &f1);
+    printf("Enter fibonacci: ");
+    scanf("%d", &f2);
+
+    printf("%llu, %llu", factorial(f1), fibonacci(f2));
+}
