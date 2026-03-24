490474759ae09f66bac62768e1dad064d8082d0d
amundgaard09 <nomispus@icloud.com>
2026-03-24 13:48:53 +0100

    Rusteze Startup

---
+++ Rusteze/target/debug/deps/firstrust.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\.vscode\AmundWorks\amundworks\Rusteze\target\debug\deps\firstrust.d: src\main.rs
+
+C:\Users\Administrator\.vscode\AmundWorks\amundworks\Rusteze\target\debug\deps\firstrust.exe: src\main.rs
+
+src\main.rs:
---
+++ Rusteze/target/debug/firstrust.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\.vscode\AmundWorks\amundworks\Rusteze\target\debug\firstrust.exe: C:\Users\Administrator\.vscode\AmundWorks\amundworks\Rusteze\src\main.rs
