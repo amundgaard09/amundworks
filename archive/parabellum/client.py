25ab09c859b0d0db0ca0b83dd9683275f3785953
amundgaard09 <nomispus@icloud.com>
2026-06-29 22:14:35 +0200

    Renaming stuff

--- archive/parabellum/client.py
+++ archive/parabellum/client.py
@@ -5,17 +5,19 @@ from durapy import uniCLI
 SERVER_IP = '192.168.0.107'
 PORT = 5000
 
-def initialize_server_connection(ip: str, port: int) -> socket.socket:
+def connect_to_server(ip: str, port: int) -> socket.socket:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         client.connect((ip, port))
-        uniCLI.console_print("CLIENT", "blue", "Connected.", "green")
-        return client
-    
+        
     except (ConnectionRefusedError, TimeoutError) as e:
         uniCLI.console_print("CLIENT", "blue", f"Failed to connect: {e}", "red")
         return None
+    
+    else:
+        uniCLI.console_print("CLIENT", "blue", "Connected", "green")
+        return client
 
 def connection_loop() -> None:
     while True:
@@ -30,7 +32,7 @@ def connection_loop() -> None:
         data = client.recv(1024).decode()
         uniCLI.console_print(f"SERVER: {SERVER_IP}", "green", data)
 
-client = initialize_server_connection(SERVER_IP, PORT)
+client = connect_to_server(SERVER_IP, PORT)
     
 if client:  
     connection_loop()  
